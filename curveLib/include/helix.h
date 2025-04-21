a3d75b21d9ba1af3ebcec16d9d40777cf885c6f0
mixx99 <maxkriv1@gmail.com>
2025-04-21 20:17:21 +0300

    first commit

---
+++ curveLib/include/helix.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "curve.h"
+
+namespace CHR{
+  class Helix : public Curve{
+    double r, step;
+    void checkValidRadius();
+  public:
+    Helix(double r_, double step_);
+    ~Helix();
+    Point getPoint(double t) const noexcept override;
+    Point getDerivative(double t) const noexcept override;
+    double getRadius() const noexcept { return r; }
+    double getStep() const noexcept { return step; }
+    void setRadius(double r_);
+    void setStep(double step_) noexcept { step = step_; }
+  };
+}
