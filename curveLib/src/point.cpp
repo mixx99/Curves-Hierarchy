ab9d6bb8390851d85df07fb77f6fce3699989975
mixx99 <maxkriv1@gmail.com>
2025-04-23 16:25:16 +0300

    updated format printing point

--- curveLib/src/point.cpp
+++ curveLib/src/point.cpp
@@ -1,6 +1,6 @@
 #include "../include/point.h"
 
 std::ostream& CHR::operator<<(std::ostream& os, const CHR::Point& point){
-    os << point.x << ' ' << point.y << ' ' << point.z;
+    os << "x = " << point.x << '\t' << "y = "  <<point.y << '\t' << "z = " << point.z;
     return os;
 }
